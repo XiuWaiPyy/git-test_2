fdc2a2d0d8b3c664b13ad11d130e38c05941cf09
pyy <pyy@qq.com>
2022-10-24 10:18:07 +0800

    commit_2

--- src/main/java/pers/yhj/git/GitTest.java
+++ src/main/java/pers/yhj/git/GitTest.java
@@ -3,5 +3,6 @@ package pers.yhj.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git_1");
+        System.out.println("hello git_2");
     }
 }
